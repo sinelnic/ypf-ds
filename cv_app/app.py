d7a2a22228b9f5e9d11fbbac86fae0f0d0a03c19
jpaulini <jpaulini@gmail.com>
2018-09-20 12:26:51 -0300

    agregado del listado de archivos en images/

--- cv_app/app.py
+++ cv_app/app.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import reqparse, abort, Api, Resource
 
 from imageai.Detection import ObjectDetection
@@ -11,14 +11,24 @@ execution_path = os.getcwd()
 app = Flask(__name__)
 api = Api(app)
 
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path , "models/resnet50_coco_best_v2.0.1.h5"))
-detector.loadModel()
-
 # argument parsing
 parser = reqparse.RequestParser()
 parser.add_argument('query')
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class Filer(Resource):    
+    def get(self):
+        """Endpoint to list files on the server."""
+        files = []
+        dir = os.path.join(execution_path, "images")
+        for filename in os.listdir(dir):
+            path = os.path.join(dir , filename)
+            if os.path.isfile(path):
+                files.append(filename)
+        return jsonify(files)
 
 class DetectObjects(Resource):
     def get(self):
@@ -27,20 +37,23 @@ class DetectObjects(Resource):
         user_query = args['query']
 
         #Detect objects in image
-        detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "images/image.jpg"), output_image_path=os.path.join(execution_path , "images/image-out.jpg"))
+        detector = ObjectDetection()
+        detector.setModelTypeAsRetinaNet()
+        detector.setModelPath( os.path.join(execution_path , "models/resnet50_coco_best_v2.0.1.h5"))
+        detector.loadModel()
+
+        detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "images/image.jpg"), 
+                                               output_image_path=os.path.join(execution_path , "images/image-out.jpg"))
 
         # create JSON object
-        output = json.dumps(detections)
-
-        for eachObject in detections:
-            print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
-
-        return output
+        return jsonify(detections)
 
 
 # Setup the Api resource routing here
 # Route the URL to the resource
-api.add_resource(DetectObjects, '/')
+
+api.add_resource(Filer, '/')
+api.add_resource(DetectObjects, '/detect')
 
 
 if __name__ == '__main__':
